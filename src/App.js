f7e9abe8e7c40254fdfe4881bd829dc7aaf67e81
SrinivasaRao <seenu4p@gmail.com>
2020-12-17 20:35:16 +0530

    Add header component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import {Header} from './Components/Header/Header';
+import './styles1.css';
+
+function App() {
+  return (
+    <div className="App">
+     {/*<h1>Welcome Sriram spacex</h1>*/}
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles2.css';
+
+export class Header extends  React.Component{
+    render(){
+        return(
+                <div className="header">
+                   <h1> Welcome Spacex Header...!! </h1>
+                </div>
+        );
+    }
+}
+
